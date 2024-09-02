9817d545cdb101f609b48fd00246b4c0009e4831
gabwowce <134537965+gabwowce@users.noreply.github.com>
2024-09-02 19:54:20 +0300

    register popup updated

--- src/components/RegisterPopup.js
+++ src/components/RegisterPopup.js
@@ -1,15 +1,16 @@
 // src/components/RegisterPopup.js
 import React from 'react';
 import { useHeaderContext } from '../context/HeaderContext';
+import '../styles/registerPopup.css';
 
 function RegisterPopup() {
   const { isPopupOpen, closePopup } = useHeaderContext();
   if (!isPopupOpen) return null; 
 
   return (
-    <div className="popup" style={styles.overlay}>
-      <div className="popup-content" style={styles.popup}>
-        <button style={styles.closeButton} onClick={closePopup}>X</button>
+    <div className="popup" >
+      <div className="popup-content" >
+        <button className="x-btn" onClick={closePopup}>X</button>
         <h2>Register</h2>
         <form>
           <label>
@@ -32,36 +33,8 @@ function RegisterPopup() {
       </div>
     </div>
   );
-}
-
-const styles = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  popup: {
-    backgroundColor: '#fff',
-    padding: '20px',
-    borderRadius: '5px',
-    width: '300px',
-    position: 'relative',
-  },
-  closeButton: {
-    position: 'absolute',
-    top: '10px',
-    right: '10px',
-    backgroundColor: 'transparent',
-    border: 'none',
-    fontSize: '16px',
-    cursor: 'pointer',
-  },
 };
+
+
 
 export default RegisterPopup;
